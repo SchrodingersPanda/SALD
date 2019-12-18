6063d2ca12df04a3676f59a55852c10959657aa8
Alejandro Soto Z <alejandro.soto@virginiogomez.cl>
2019-12-18 17:15:23 -0300

    Agregar archivos de proyecto.

---
+++ SALD/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SALD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
